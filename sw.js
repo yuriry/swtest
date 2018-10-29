66b7babaacc470eaa58b5590c6bd1252a4f702e8
Yuri <yuriry@users.noreply.github.com>
2018-10-29 04:35:36 -0700

    Added more event handlers

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
 self.addEventListener('install', function (evt) {
     console.log('install event')
 });
+
+self.addEventListener('activate', function (evt) {
+    console.log('activate event')
+});
+
+self.addEventListener('message', function (evt) {
+    console.log('message event')
+});
+
+self.addEventListener('fetch', function (evt) {
+    console.log('fetch event')
+});
+
+self.addEventListener('sync', function (evt) {
+    console.log('sync event')
+});
+
+self.addEventListener('push', function (evt) {
+    console.log('push event')
+});
